a0795936e04a124fc1f275e702dbe28cfb5fe5d6
rafaelpllopes <rafelpllopes@gmail.com>
2018-02-09 07:45:22 -0200

    Terminando de implementar o calculo de horas.

--- pontos/src/app/lista-pontos/registros/registros.component.ts
+++ pontos/src/app/lista-pontos/registros/registros.component.ts
@@ -66,20 +66,46 @@ export class RegistrosComponent implements OnInit, OnDestroy, OnChanges {
       horas.push(`${div[0]}h ${div[1]}m`);
     });
     let result = this.calculaHoras(horas);
-    return `${parseInt(result.h)}:${result.m}`;
+    return result ? `${this.pad(parseInt(result.h), 2)}:${this.pad(result.m,2)}`:result;
   }
 
   calculaHoras(horas) {
     let calc = [];
-    let ref: number = 0;
-    let result: any;
-    if(horas.length % 2 == 0) {
-      for(let i = 0; i < horas.length; i++){
-        calc.push(this.hmh.diff(horas[ref], horas[ref+1], 'hours'));
-        console.log(calc);
-        ref += 2;
+    let result;
+
+    switch(horas.length){
+      case 2:
+      case 3: {
+        result = this.hmh.diff(horas[0], horas[1], 'hours');
+        return result;
       }
-      return this.hmh.sum(calc[0], calc[1]);
+      case 4:
+      case 5: {
+        calc.push(this.hmh.diff(horas[0], horas[1], 'hours'));
+        calc.push(this.hmh.diff(horas[2], horas[3], 'hours'));
+        result = this.hmh.sum(`${this.pad(parseInt(calc[0].h), 2)}h ${calc[0].m}m ${this.pad(parseInt(calc[1].h), 2)}h ${calc[1].m}m`, 'hours');
+        console.log(`${this.pad(parseInt(calc[0].h), 2)}h ${calc[0].m}m ${this.pad(parseInt(calc[1].h), 2)}h ${calc[1].m}m`);
+        return result;
+      }
+      case 6:
+      case 7: {
+        calc.push(this.hmh.diff(horas[0], horas[1], 'hours'));
+        calc.push(this.hmh.diff(horas[2], horas[3], 'hours'));
+        calc.push(this.hmh.diff(horas[4], horas[5], 'hours'));
+        result =  this.hmh.sum(`${this.hmh.sum(`${this.pad(parseInt(calc[0].h), 2)}h ${calc[0].m}m ${this.pad(parseInt(calc[1].h), 2)}h ${calc[1].m}m`, 'hours')}
+          ${this.pad(parseInt(calc[2].h), 2)}h ${calc[2].m}m           `
+        ,'hours');
+        return result;
+      }
+      default: {
+        return '';
+      }
     }
+  }
+
+  pad(num, size) {
+    let s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
   }
 }
